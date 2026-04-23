ae13587aa687f189429a9642aacf6e94c8a695d2
Maksim <maksim.tihhomirov@techno.ee>
2026-04-23 11:29:02 +0300

    Episode 5 - JV types

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ alert("Maksim");
 console.log("JavaScript is working");
 
 document.querySelector('p').style.color = 'red'; 
+
+console.log("hello"); 
+const firstName = 'Maksim';
+const lastName = 'Tihhomirov';
+const jobTitle = 'Web Developer';
+
+// обычное соединение строк
+const fullName = firstName + ' ' + lastName;
+const about = 'Name: ' + fullName + ', Job: ' + jobTitle;
+
+// template strings (правильный современный способ)
+const fullName2 = `${firstName} ${lastName}`;
+const about2 = `Name: ${fullName2}, Job: ${jobTitle}`;
+
+console.log(about);
+console.log(about2);
