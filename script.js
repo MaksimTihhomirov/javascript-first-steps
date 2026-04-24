ca7e08fce92c0435e868e2d74d6e4e0f5f99c896
Maksim <maksim.tihhomirov@techno.ee>
2026-04-24 23:18:15 +0300

    Numbers and BigInts

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-alert("Maksim");
-console.log("JavaScript is working");
+/* Variables */
+const num1 = 4;
+const num2 = 2;
+const num3 = 4.5;
+const num4 = -4.5;
+const myvar ='A string';
+const myvar2 ='Another string';
 
-document.querySelector('p').style.color = 'red'; 
+/* Basic Math
+Print our results
+*/
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
 
-console.log("hello"); 
-const firstName = 'Maksim';
-const lastName = 'Tihhomirov';
-const jobTitle = 'Web Developer';
+// Floating point inaccuracy
+console.log(0.1 + 0.2);
+console.log(0.2 + 0.4);
 
-// обычное соединение строк
-const fullName = firstName + ' ' + lastName;
-const about = 'Name: ' + fullName + ', Job: ' + jobTitle;
+console.log(num1 < num2);
+console.log(num1 <= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2);
+console.log(num1 !== num2);
 
-// template strings (правильный современный способ)
-const fullName2 = `${firstName} ${lastName}`;
-const about2 = `Name: ${fullName2}, Job: ${jobTitle}`;
+// NaN
+console.log(num1 * 'A string');
 
-console.log(about);
-console.log(about2);
+// Infinity
+console.log(9_000_000 ** 9_000_000);
+console.log(Number.NEGATIVE_INFINITY);
+
+// Math Object
+console.log(Math.PI);
+console.log(Math.sqrt(25));
+console.log(Math.pow(2,3));
+console.log(Math.round(6.6));
+console.log(Math.round(6.3));
+console.log(Math.ceil(6.3));
+console.log(Math.floor(6.7));
+
+// Converting
+console.log(myVar + myVar2);
+console.log(myVar + num1);
+
+// String to number
+console.log(parseInt("2") + 2);
+
+// Number to string
+console.log(2 + '');
+console.log(`${2}`);
+
+// Bigint - INtroduced ES2020
+let maxint = Bigint(Number.MAX_SAFE_INTEGER);
+console.log(maxint);
+maxInt++ ;
+console.log(maxInt);
+
+let bigNumber = 9007199254740993n;
+bigNumber = bigNumber * 4n;
+console.log(bigNumber);
