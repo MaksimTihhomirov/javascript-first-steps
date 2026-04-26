beacad1959bc41829e41d8d4d7b0e89a5f7c55ac
Maksim <maksim.tihhomirov@techno.ee>
2026-04-26 16:58:47 +0300

    Save local files before pull

---
+++ episode 8/script.js
@@ -0,0 +1,39 @@
+let age = 12;
+
+const is0ld = age >=30;
+const isCool = false;
+
+// console.log(is0ld);
+// console.log(isCool);
+
+// Comparisons
+
+// console.log(4 > 2);
+// console.log(4 >= 2);
+// console.log(4 > 2);
+// console.log(4 <= 2);
+
+// Equality: Strict Equality === vs Loose Equality ===
+console.log(4 === '4');
+console.log(4 == '4');
+console.log(4 !== '4');
+console.log(4 != '4');
+
+// Logical Operators
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// console.log(isOld || isCool)
+
+// Convert boolean to a string
+// console.log(crossOriginIsolated.toString());
+
+// Trutry vs Falsy
+console.log(Boolean(NaN));
