3b1337ed91364286457c3ab64d747615b876af78
Maksim <maksim.tihhomirov@techno.ee>
2026-05-03 22:26:39 +0300

    Understanding Scope

---
+++ Understanding Scope/script.js
@@ -0,0 +1,64 @@
+// Global Scope 
+// let message = 'hello';
+// var greeting = 'hi'; 
+
+// console.log(message);
+// console.log(greeting);
+
+// if (true) {
+// console.log(message);
+// console.log(greeting);
+// }
+
+// Function Scope
+// function sayHello() {
+//     let message = 'hello';
+//     var greeting = 'hi';
+
+//     console.log(message);
+//     console.log(greeting);
+
+//     function nested() {
+//     console.log(message);
+//     console.log(greeting);
+//     }
+
+//     nested();
+// }
+
+
+
+// sayHello();
+
+
+
+// Block Scope
+
+// if (true) {
+//   let message = 'hello';
+//   var greeting = 'hi';
+
+
+//   console.log(message);
+//   console.log(greeting);
+// }
+
+// console.log(greeting);
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(`i is ${i}`);
+// }
+
+// console.log(i);
+
+// const alert = 'error';
+
+(function() {
+    const alert = 'error';
+    console.log(alert);
+})();
+
+alert('hi');
+
+
+
