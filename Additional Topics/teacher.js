8e727037a93de48063d00a595a77997434355cc6
Maksim <maksim.tihhomirov@techno.ee>
2026-05-07 23:08:21 +0300

    Additional Topics

---
+++ Additional Topics/teacher.js
@@ -0,0 +1,17 @@
+class Student {
+    name;
+    year;
+
+    constructor(name, year) {
+        this.name = name;
+        this year = year;
+    }
+
+introduceSelf() {
+    console.log(`Hi, My name is ${this.name}.`);
+
+}
+Homework() {
+    console.log('Doing homework'); 
+ }
+}
